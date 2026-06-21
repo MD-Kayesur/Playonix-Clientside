303254252f9355c6280bb284b365df8a6ceb0e81
MD-Kayesur <rmdkayesur@gmail.com>
2026-06-21 15:31:16 +0600

    Remove empty line from temp_auto_push.bat

--- src/redux/hooks/redux-hook.ts
+++
@@ -1,8 +0,0 @@
-import { AppDispatch, RootState } from "@/store/store";
-import { useDispatch, useSelector } from "react-redux";
-
-const useAppDispatch = useDispatch.withTypes<AppDispatch>();
-const useAppSelector = useSelector.withTypes<RootState>();
-
-export { useAppDispatch, useAppSelector };
-
